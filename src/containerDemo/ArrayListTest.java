77f974e8e1a0505f124c43ebd19e5da831707d9b
wukun <wukun@hlchang.cn>
2019-04-23 11:36:49 +0800

    .

--- src/containerDemo/ArrayListTest.java
+++ src/containerDemo/ArrayListTest.java
@@ -1,5 +1,6 @@
-package arraylist;
+package containerDemo;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class ArrayListTest
@@ -40,3 +41,37 @@ public class ArrayListTest
   }
 }
 
+class Employee
+{
+    private String name;
+    private double salary;
+    private LocalDate hireDay;
+
+    public Employee(String n, double s, int year, int month, int day)
+    {
+        name = n;
+        salary = s;
+        hireDay = LocalDate.of(year, month, day);
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public double getSalary()
+    {
+        return salary;
+    }
+
+    public LocalDate getHireDay()
+    {
+        return hireDay;
+    }
+
+    public void raiseSalary(double byPercent)
+    {
+        double raise = salary * byPercent / 100;
+        salary += raise;
+    }
+}
